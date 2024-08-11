298113e004290aefd3c8388c979ec6b212bf7800
Artem Rybin <catobazz@gmail.com>
2024-08-11 21:16:51 +0300

    lib start

---
+++ dist/index.cjs
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+
+const jsxRuntime = require('react/jsx-runtime');
+
+function Button(props) {
+  return /* @__PURE__ */ jsxRuntime.jsx("button", { ...props, children: "Hello world" });
+}
+
+exports.Button = Button;
