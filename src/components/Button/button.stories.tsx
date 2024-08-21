f9aa461a9dab1a37dccf380a9f8a4100abee7ee5
Artem Rybin <catobazzz@gmail.com>
2024-08-21 14:44:03 +0300

    add button with ref and without ref

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,6 +1,6 @@
-import { Button } from './button';
+import {Button} from './button';
 import {Meta, StoryObj} from '@storybook/react';
-import {useState} from "react";
+import {useRef, useState} from "react";
 import {render} from "react-dom";
 
 const meta = {
@@ -27,10 +27,54 @@ export const Secondary: Story = {
 export const FullWidth: Story = {
     args: {
         fullWidth: true,
-        children: 'FullWidth',
+        children: 'Full Width',
     },
     render: (args) => {
-      const [count, setCount] = useState(0);
-      return <Button {...args} onClick={() => setCount(count + 1)}>{count}</Button>;
+        const [count, setCount] = useState(0);
+        return <Button {...args} onClick={() => setCount(count + 1)}>{count}</Button>;
     }
 };
+
+export const Link: Story = {
+    args: {
+        fullWidth: true,
+        children: 'Link',
+    },
+    render: (args) => {
+        return (
+            <Button
+                {...args} asChild>
+                <
+                    a href="https://www.google.com" target="_blank" title={"google"}> Go to Google
+                </a>
+            </Button>);
+    }
+};
+
+// export const Link: Story = {
+//     args: {
+//         fullWidth: true,
+//         children: 'Link',
+//     },
+//     render: (args) => {
+//         const buttonRef = useRef<HTMLButtonElement>(null)
+//         const anchorRef = useRef<HTMLAnchorElement>(null)
+//         return (
+//             <div><Button
+//                 {...args} asChild>
+//                 <
+//                     a href="https://www.google.com" target="_blank" title={"google"} ref={anchorRef}> Go to Google
+//                 </a>
+//             </Button>
+//                 <Button
+//                     {...args}
+//                     ref={buttonRef}
+//                     onClick={() => anchorRef.current?.click()}
+//                 >
+//                     Nice Button
+//                 </Button>
+//             </div>
+//         );
+//
+//     }
+// }; вариант с рефом
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,13 +1,43 @@
-import { ComponentPropsWithoutRef } from "react";
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react";
 import s from './button.module.css'
 import clsx from "clsx";
+import {Slot} from "@radix-ui/react-slot";
 
 type Props = ComponentPropsWithoutRef<"button"> & {
-  variant?: "primary" | "secondary";
-  fullWidth?: boolean;
+    asChild?: boolean;
+    variant?: "primary" | "secondary" | "outlined" | "ghost";
+    fullWidth?: boolean;
 };
 
-export function Button({ variant = "primary", fullWidth, ...props }: Props) {
-  console.log("button");
-  return <button {...props} className={clsx(s.button, s[variant], fullWidth && s.fullWidth)}></button>;
+export function Button(
+    {
+        variant = "primary",
+        asChild,
+        fullWidth,
+        ...props
+    }: Props) {
+
+    const Comp = asChild ? Slot : 'button';
+    return (
+        <Comp
+            {...props}
+            className={clsx(s.button, s[variant], fullWidth && s.fullWidth)}
+        />)
 }
+
+// export const Button = forwardRef<ElementRef<"button">, Props>((
+//     {
+//         variant = "primary",
+//         asChild,
+//         fullWidth,
+//         ...props
+//     }, ref) => {
+//
+//     const Comp = asChild ? Slot : 'button';
+//     return (
+//         <Comp
+//             {...props}
+//             ref={ref}
+//             className={clsx(s.button, s[variant], fullWidth && s.fullWidth)}
+//         />)
+// }) кнопка с рефами
